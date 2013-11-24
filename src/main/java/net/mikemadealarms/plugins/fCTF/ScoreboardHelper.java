2e089ac0b2f525f3334b81453c20687facda2dd0
EthanReg <EthanReg@EthanReg-PC>
2013-11-24 09:59:16 -0500

    Added ScoreboardHelper.

--- src/main/java/net/mikemadealarms/plugins/fCTF/ScoreboardHelper.java
+++ src/main/java/net/mikemadealarms/plugins/fCTF/ScoreboardHelper.java
@@ -2,6 +2,10 @@
  * Copyright © 2013 Ethan White and Mike Stegne
  */
 package net.mikemadealarms.plugins.fCTF;
+
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
 public class ScoreboardHelper {
     public static ScoreboardHelper instance;
     public ScoreboardHelper() {
