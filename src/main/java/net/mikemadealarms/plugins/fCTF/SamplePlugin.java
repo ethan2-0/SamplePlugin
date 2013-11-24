2171c6f6e1e440c26f4f5a6f113d09defde82b14
EthanReg <EthanReg@EthanReg-PC>
2013-11-24 09:00:29 -0500

    Added a copyright,

--- src/main/java/net/mikemadealarms/plugins/fCTF/SamplePlugin.java
+++ src/main/java/net/mikemadealarms/plugins/fCTF/SamplePlugin.java
@@ -8,32 +8,16 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Sample plugin for Bukkit
- *
- * @author Dinnerbone
+ * Copyright © 2013 Ethan White and Mike Stegne
  */
 public class SamplePlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // TODO: Place any custom disable code here
-
-        // NOTE: All registered events are automatically unregistered when a plugin is disabled
-
-        // EXAMPLE: Custom code, here we just output some info so we can check all is well
         getLogger().info("Goodbye world!");
     }
-
     @Override
     public void onEnable() {
-        //getLogger().info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
-    }
-
-    public boolean isDebugging(final Player player) {
-        return false;
-    }
-
-    public void setDebugging(final Player player, final boolean value) {
         
     }
 }
