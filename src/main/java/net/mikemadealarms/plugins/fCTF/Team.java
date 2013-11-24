0f48551bd9f5c9aea3b715bc4d28a95c96dcc88e
EthanReg <EthanReg@EthanReg-PC>
2013-11-24 10:15:28 -0500

    Added ScoreboardHelper.

--- src/main/java/net/mikemadealarms/plugins/fCTF/Team.java
+++ src/main/java/net/mikemadealarms/plugins/fCTF/Team.java
@@ -3,6 +3,29 @@
  */
 
 package net.mikemadealarms.plugins.fCTF;
+
+import java.util.ArrayList;
+
 public class Team {
-    
+    String prefix;
+    ArrayList<fPlayer> players;
+    public Team(String prefix) {
+        this.prefix = prefix;
+        players = new ArrayList<fPlayer>();
+    }
+    public boolean hasPlayer(fPlayer p) {
+        for(int i = 0; i < players.size(); i++) {
+            if(players.get(i) == p) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean addPlayer(fPlayer p) {
+        if(hasPlayer(p)) {
+            return false;
+        }
+        players.add(p);
+        return true;
+    }
 }
